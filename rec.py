bb928025b9b1f594f12a2800b77a6a672dd75803
Nakata <4ryo2-meron-math@ezweb.ne.jp>
2019-09-14 06:14:26 +0900

    add get value from table

--- rec.py
+++ rec.py
@@ -15,13 +15,25 @@ def getURLText(URL, auth):
     get_url_info = requests.get(URL, auth=("mpcman", "1958"))
     return get_url_info.text
 
+def getValList(tableText):
+    sp = text.split("table")
+    # sp[2]が正しい
+    valLL = sp[5].split("style_td\">")
+    val = [v.split("</td")[0] for v in valLL]
+
+    lst = []
+    for i in range(7,len(val), 4):
+        if "gt" in val[i]:
+            lst.append(int(val[i].split(";")[1]))
+        else:
+            lst.append(int(val[i]))
+    return lst
+
 
 if __name__ == "__main__":
 
     url, id, pas = readCSV()
     text = getURLText(url, (id, pas))
+    val = getValList(text)
 
-    sp = text.split("table")
-
-    # sp[2]が正しい
-    print(sp[5])
+    print(val)
